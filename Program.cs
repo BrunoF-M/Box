a5116fdefa26b042fbfd0fd2125e0334cd411378
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-12 02:24:53 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aperte enter Porfavor...");
+            Console.ReadLine();
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("|   Bruno   |");
+            Console.WriteLine("+-----------+");
+            Console.WriteLine(" "); //espaço 
+            Console.WriteLine("Obrigado =)!");
+            Console.WriteLine("aperte enter para finalizar..");
+            Console.ReadLine();
+        }
+    }
+}
